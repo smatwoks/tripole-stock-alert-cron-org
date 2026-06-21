86b3488af17a430a4aa2ff213ee591f9dada1917
Peddi reddy Bhumireddy <113705525+smatwoks@users.noreply.github.com>
2026-06-21 13:14:35 +0530

    Add root route to confirm server is running
    Added a root route that responds with 'Server Running'.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
 
 app.get("/check-stock", async (req, res) => {
   try {
